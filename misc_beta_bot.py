ee71052027bf3b2a7e8404103240e217a3ab0d02
S1S13AF7 <S1S13AF7@MISC.PP.UA>
2025-02-03 18:51:22 +0200

    Add files via upload

--- misc_beta_bot.py
+++ misc_beta_bot.py
@@ -29,11 +29,9 @@ if db_sqlite3:
 	cur.execute('''CREATE TABLE IF NOT EXISTS users (
 	user_id	INTEGER NOT NULL DEFAULT 0 UNIQUE,
 	reg_int	INTEGER NOT NULL DEFAULT 0,
-	f_name	VARCHAR NOT NULL DEFAULT 'хз',
-	mcoins	INTEGER NOT NULL DEFAULT 1024,
-	rnd_kd	INTEGER NOT NULL DEFAULT 0,
+	f_name	VARCHAR NOT NULL DEFAULT '',
 	lng_code	VARCHAR NOT NULL DEFAULT ''
-	)''');
+	);''');
 	con.commit()
 
 if db_pymysql:
@@ -48,27 +46,12 @@ if db_pymysql:
 	dbc.execute('''CREATE TABLE IF NOT EXISTS `tg_bot_users` (
 	`user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
 	`reg_int` int(11) unsigned NOT NULL DEFAULT '0',
-	`f_name` text NOT NULL,
-	`mcoins` bigint(20) unsigned NOT NULL DEFAULT '1024',
-	`rnd_kd` int(11) unsigned NOT NULL DEFAULT '0',
+	`f_name` text CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
 	`lng_code` varchar(8) NOT NULL DEFAULT '',
 	PRIMARY KEY (`user_id`)
 	);''');
 	ldb.commit()
 	#bot users.
-	dbc.execute('''CREATE TABLE IF NOT EXISTS `tg_iris_zarazy` (
-	`when_int` int(11) unsigned NOT NULL DEFAULT '0',
-	`who_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-	`user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-	`u_link` varchar(500) NOT NULL DEFAULT '',
-	`bio_str` varchar(11) NOT NULL DEFAULT '1',
-	`bio_int` int(11) unsigned NOT NULL DEFAULT '1',
-	`expr_int` int(11) unsigned NOT NULL DEFAULT '0',
-	`expr_str` varchar(11) NOT NULL DEFAULT '0',
-	UNIQUE KEY `UNIQUE` (`who_id`,`user_id`)
-	);''');
-	#зберігалка: https://github.com/S1S13AF7/ub4tg
-	ldb.commit()
 
 async def reg_user(message: types.Message):
 	print(message)
@@ -77,7 +60,7 @@ async def reg_user(message: types.Message):
 	lng_code = message.from_user.language_code or ''
 	when_int = int(datetime.timestamp(message.date))
 	reg_date = 0
-	mcoins_c = 0
+	
 	if db_sqlite3:
 		try:
 			cur.execute("INSERT OR IGNORE INTO users(user_id,reg_int,f_name,lng_code) VALUES (?,?,?,?)", (int(user_id),int(when_int),str(user_fn),str(lng_code))); con.commit()
@@ -85,21 +68,18 @@ async def reg_user(message: types.Message):
 			print(f"sqlite INSERT:{Err}")
 		
 		sqlite_rd=when_int#for min (rd,rd)
-		sqlite_co=1024
+		
 		try:
-			cur.execute("SELECT reg_int,mcoins FROM users WHERE user_id = %d" % int(user_id)); 
+			cur.execute("SELECT reg_int FROM users WHERE user_id = %d" % int(user_id)); 
 			rd = cur.fetchone();
 			if rd is None:
 				print('не знайшли юзера у базі sqlite')
 			else:
 				sqlite_rd=int(rd[0])
-				sqlite_co=int(rd[1])
 				if sqlite_rd > 0:
 					reg_date=sqlite_rd
 				else:
 					reg_date=when_int
-				if sqlite_co > 0:
-					mcoins_c=sqlite_co
 		except Exception as Err:
 			print(f"sqlite SELECT:{Err}")
 	
@@ -109,39 +89,34 @@ async def reg_user(message: types.Message):
 		except Exception as Err:
 			print(f"localhost INSERT:{Err}")
 		pymysql_rd=when_int#for min (rd,rd)
-		pymysql_co=1024
 		try:
-			dbc.execute("SELECT reg_int,mcoins FROM `tg_bot_users` WHERE user_id = %d" % int(user_id)); 
+			dbc.execute("SELECT reg_int FROM `tg_bot_users` WHERE user_id = %d" % int(user_id)); 
 			rd = dbc.fetchone();
 			if rd is None:
 				print('не знайшли юзера у базі localhost')
 			else:
 				pymysql_rd=int(rd['reg_int'])
-				pymysql_co=int(rd['mcoins'])
 				if pymysql_rd > 0:
 					reg_date=pymysql_rd
 				else:
 					reg_date=when_int
-				if pymysql_co > 0:
-					mcoins_c=pymysql_co
 		except Exception as Err:
 			print(f"localhost SELECT:{Err}")
 
 	if db_sqlite3 and db_pymysql:
 		#якщо юзаємо обидві бази,то
 		reg_date=int(min(sqlite_rd,pymysql_rd))
-		mcoins_c=int(max(sqlite_co,pymysql_co))
 		
-		if sqlite_rd < pymysql_rd or sqlite_co > pymysql_co:
-			#UPDATE `tg_bot_users` SET `reg_int`=?,`mcoins`=? WHERE `user_id`=?;
+		if sqlite_rd < pymysql_rd:
+			#UPDATE `tg_bot_users` SET `reg_int`=? WHERE `user_id`=?;
 			try:
-				dbc.execute(f"UPDATE `tg_bot_users` SET `reg_int` ='{reg_date}',`mcoins` ='{mcoins_c}' WHERE user_id = %d" % int(user_id)); ldb.commit()#як я хотів воно нехотіло, тому буде пока так.
+				dbc.execute(f"UPDATE `tg_bot_users` SET `reg_int` ='{reg_date}' WHERE user_id = %d" % int(user_id)); ldb.commit()#як я хотів воно нехотіло, тому буде пока так.
 			except Exception as Err:
 				print(f"localhost UPDATE:{Err}")
 		
-		if sqlite_rd > pymysql_rd or sqlite_co < pymysql_co:
+		if sqlite_rd > pymysql_rd:
 			try:
-				cur.execute("UPDATE users SET reg_int =?,mcoins=? WHERE user_id=?", (int(reg_date),int(mcoins_c),int(user_id))); con.commit()
+				cur.execute("UPDATE users SET reg_int =? WHERE user_id=?", (int(reg_date),int(user_id))); con.commit()
 			except Exception as Err:
 				print(f"sqlite UPDATE:{Err}")
 	
@@ -182,79 +157,6 @@ async def cmd_reg(message: types.Message):
 	elif rd > 0:
 		snd_msg = time.strftime('%d.%m.%Y', time.localtime(rd))
 	await message.answer(snd_msg)
-
-@dp.message_handler(commands=['mz','мж','мз'])
-async def cmd_myzh (message: types.Message):
-	msg="🤷"
-	user_id = int(message.from_user.id)
-	user_fn = message.from_user.first_name or ''
-	lng_code = message.from_user.language_code or ''
-	when_int = int(datetime.timestamp(message.date))
-	rd=int(await reg_user(message))#create or date
-	if db_pymysql:
-		ii=0
-		try:
-			
-			dbc.execute("SELECT user_id,bio_str,expr_int,when_int FROM `tg_iris_zarazy` WHERE who_id = %d ORDER BY when_int DESC LIMIT 20;" % int(user_id));
-			bz_info = dbc.fetchmany(20)#получить
-			all_sicknes=[]#інфа
-			count=len(bz_info)
-			who=f'🦠 <a href="tg://openmessage?user_id={user_id}">{user_fn}</a>:'
-			for row in bz_info:
-				ii+=1
-				print(row)
-				id_user=row["user_id"]
-				bio_str=row["bio_str"]
-				u_link =f'tg://openmessage?user_id={id_user}'	#fix для любителів мінять його
-				a_href = f'<a href="{u_link}"><code>@{id_user}</code></a>'
-				if int(row["when_int"]) < 1735682400:	#01.01.2025 00:00:00
-					all_sicknes.append(f"{ii}.	{a_href}	<s>➕{bio_str}</s>\n")	#	старі закреслені. 
-				else:
-					all_sicknes.append(f"{ii}.	{a_href}	➕{bio_str}\n")	# якщо будуть нові?
-			if len(all_sicknes)!=0:
-				all_sicknes=f'{who}\n{"".join(all_sicknes)}'
-			else:
-				all_sicknes='🤷 інфа нема.'
-			msg=all_sicknes
-		except Exception as Err:
-			msg = Err
-			print(f"localhost SELECT:{Err}")
-	await message.answer(msg, parse_mode=types.ParseMode.HTML)
-
-@dp.message_handler(commands=['ends'])
-async def cmd_ends (message: types.Message):
-	user_id = int(message.from_user.id)
-	user_fn = message.from_user.first_name or ''
-	lng_code = message.from_user.language_code or ''
-	when_int = int(datetime.timestamp(message.date))
-	rd=int(await reg_user(message))#create or date
-	msg="<code>Биослет</code>"
-	if when_int<1735682400:
-		if db_pymysql:
-			ii=0
-			try:
-				dbc.execute(f"SELECT user_id,bio_str,expr_str FROM `tg_iris_zarazy` WHERE who_id = {user_id} AND expr_int < {when_int} ORDER BY `bio_int` DESC, `when_int` DESC LIMIT 20;")
-				bz_info = dbc.fetchmany(20)#получить
-				all_sicknes=[]#інфа
-				count=len(bz_info)
-				who=f'🦠 <a href="tg://openmessage?user_id={user_id}">{user_fn}</a>:'
-				for row in bz_info:
-					print(row)
-					ii+=1
-					id_user=row["user_id"]
-					bio_str=row["bio_str"]
-					u_link =f'tg://openmessage?user_id={id_user}'
-					a_href = f'<a href="{u_link}"><code>@{id_user}</code></a>'
-					all_sicknes.append(f"{ii}.	{a_href}	<s>➕{bio_str}</s>\n")
-				if len(all_sicknes)!=0:
-					all_sicknes=f'{who}\n{"".join(all_sicknes)}'
-				else:
-					all_sicknes=msg="<code>Биослет</code>"
-				msg=all_sicknes
-			except Exception as Err:
-				msg = Err
-				print(f"localhost SELECT:{Err}")
-	await message.answer(msg, parse_mode=types.ParseMode.HTML)
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
